3d2b3596dcba3ce60e3b1dcf2878d8a0d644b7cb
nathandbentley <nathandbentley@gmail.com>
2026-09-06 00:09:29 -0500

    Update AccountWvwClient.cs

--- Gw2Sharp/WebApi/V2/Clients/Account/AccountWvwClient.cs
+++ Gw2Sharp/WebApi/V2/Clients/Account/AccountWvwClient.cs
@@ -1,14 +1,34 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Gw2Sharp.WebApi;
 using Gw2Sharp.WebApi.V2.Models;
 
 namespace Gw2Sharp.WebApi.V2.Clients
 {
-    public class AccountWvwClient : BaseClient<AccountWvw>, IAccountWvwClient
+    /// <summary>
+    /// A client of the Guild Wars 2 API v2 account WvW endpoint.
+    /// </summary>
+    [EndpointPath("account/wvw")]
+    public class AccountWvwClient : BaseEndpointBlobClient<AccountWvw>, IAccountWvwClient
     {
-        public AccountWvwClient(IGw2ClientInternal connection) : base(connection)
+        /// <summary>
+        /// Creates a new <see cref="AccountWvwClient"/> that is used for the API v2 account WvW endpoint.
+        /// </summary>
+        /// <param name="connection">The connection used to make requests, see <see cref="IConnection"/>.</param>
+        /// <param name="gw2Client">The Guild Wars 2 client.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="connection"/> or <paramref name="gw2Client"/> is <see langword="null"/>.</exception>
+        protected internal AccountWvwClient(IConnection connection, IGw2Client gw2Client) :
+            base(connection, gw2Client)
         {
         }
 
-        protected override string EndpointPath => "account/wvw";
+        /// <inheritdoc />
+        public  Task<AccountWvw> GetAsync() =>
+            base.GetAsync();
+
+        /// <inheritdoc />
+        public override  Task<AccountWvw> GetAsync(CancellationToken cancellationToken) =>
+            base.GetAsync(cancellationToken);
     }
 }
